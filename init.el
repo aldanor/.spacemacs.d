41503717f568ebf267b3e1725862e9ecdf6d41b5
Ivan Smirnov <i.s.smirnov@gmail.com>
2016-03-07 23:25:34 +0000

    A few modifications to spacemacs theme

--- init.el
+++ init.el
@@ -261,7 +261,8 @@
 It is called immediately after `dotspacemacs/init'.  You are free to put almost
 any user code here.  The exception is org related code, which should be placed
 in `dotspacemacs/user-config'."
-  ;; (setq mac-option-modifier 'meta)
+  (setq spacemacs-theme-comment-bg nil
+        spacemacs-theme-org-height nil)
   )
 
 (defun dotspacemacs/user-config ()
